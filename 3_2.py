80a3741eae8a589943352dbd44d577ac4e2f3f06
milej <milejsho18@gmail.com>
2022-05-09 12:01:23 +0300

    initial commit

---
+++ 3_2.py
@@ -0,0 +1,10 @@
+first_num = float(input("Введи число:"))
+second_num = float(input("Введи число:"))
+third_num = float(input("Введи число:"))
+first_num = round(first_num, 3)
+second_num = round(second_num, 3)
+third_num = round(third_num, 3)
+summ_num = first_num + second_num + third_num
+finish_num = summ_num/3
+finish_num = round(finish_num, 3)
+print(finish_num)
